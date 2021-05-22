cb7e773033170d37109de9196c3053c5f868f4a1
Reinoud Benoot <79507784+ReinoudBenoot@users.noreply.github.com>
2021-05-22 10:23:30 +0200

    Updated a basic code example

--- scripts/Main.py
+++ scripts/Main.py
@@ -3,8 +3,8 @@ from src.Drone import *
 connection_string = 'udp:127.0.0.1:14550'
 
 drone = Drone(connection_string, alt_type="barometer")
+
 drone.arm_and_takeoff(2)
-#drone.yaw(45)
-#time.sleep(10)
+drone.go_to_target(5)
 drone.land()
 drone.disarm()
